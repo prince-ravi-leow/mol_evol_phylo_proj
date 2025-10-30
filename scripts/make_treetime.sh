95f5293e521ea163a631050972afc1b256d18d20
Prince Ravi Leow <princeravileow@outlook.com>
2025-10-30 14:26:11 +0100

    re-run iqtree + treetime on filtered msa

--- scripts/make_treetime.sh
+++ scripts/make_treetime.sh
@@ -1,9 +1,9 @@
 treetime \
---tree "data/iqtree_output/full_species_rabies_no_mf.treefile" \
---aln "data/deduplicated/align_full_no_mf.fasta" \
+--tree "data/iqtree_output/full_species_rabies_filtered.treefile" \
+--aln "data/deduplicated/align_full_filtered.fasta" \
 --rng-seed 42 \
 --dates "data/timetree/dates.csv" \
---outdir "data/timetree/rerun_no_mf" \
+--outdir "data/timetree/rerun_filtered" \
 --verbose 6
 # --relax 1.0 0 \
 # --gtr infer \
